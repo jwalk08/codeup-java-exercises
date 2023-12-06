cb94d188c463c55e00a3f0bd33bb508695ac4bd7
Janice Walker <walker.janice@aol.com>
2023-12-06 11:47:44 -0600

    HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,33 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+//            System.out.println("Hello World");
+//       int myFavoriteNumber = 7;
+//       System.out.println(myFavoriteNumber);
+//       String myString ="Today is the day";
+//       System.out.println(myString);
+//       char myStrings ='C';
+//       System.out.println(myStrings);
+//       long myNumber = 123;
+//       float myNumbers = 3.0F;
+//       double myDouble = 345.90;
+//       int x = 5;
+//       System.out.println(x++);
+//       System.out.println(x);
+//       int myInteger = 900;
+//       long morePrecise = myInteger;
+//       System.out.println(morePrecise);
+//       int v = 5;
+//       System.out.println(++v);
+//       System.out.println(v);
+//
+
+//    int x = 4;
+//    System.out.println(x +5 );
+       int x = 3;
+       int y = 4;
+       System.out.println(y * x);
+
+
+
+        }
+}
