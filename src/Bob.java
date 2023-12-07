082268eba12527bc8ad621c15856058e7162fd88
Janice Walker <walker.janice@aol.com>
2023-12-07 11:45:12 -0600

    bob

---
+++ src/Bob.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+         String userInput = sc.nextLine();
+         if (userInput.endsWith("?")) {
+             System.out.println("Sure");
+         } else if (userInput.endsWith("!")) {
+             System.out.println("Whoa, chill out!");
+         } else if (userInput.equals("")) {
+             System.out.println("Fine. Be that way!");
+         } else {
+             System.out.println("Whatever");
+         }
+
+    }
+}
